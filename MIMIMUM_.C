b70b0bba659529883446f13cf84fd6ceb0363b54
bhumikamakwana396 <bhumikamakwana396@gmail.com>
2025-05-26 22:04:46 +0530

    25-05-2025

--- MIMIMUM_.C
+++ MIMIMUM_.C
@@ -3,16 +3,16 @@
 
 void main()
 {
-	int i,a[10],min,max;
+	int i,a[10],min,max,temp,j;
 	clrscr();
-	printf("\nEnter array elements");
-	for(i=0;i<5;i++)
+	printf("\nEnter array elements\n");
+	for(i=0;i<10;i++)
 	{
 		printf("\nEnter %d row elements:",i);
 		scanf("%d",&a[i]);
 	}
 	min=a[0];
-	for(i=0;i<5;i++)
+	for(i=0;i<10;i++)
 	{
 			if(a[i]<min)
 			{
@@ -20,7 +20,7 @@ void main()
 			}
 	}
 	max=a[0];
-	for(i=0;i<5;i++)
+	for(i=0;i<10;i++)
 	{
 
 			if(a[i]>max)
@@ -28,7 +28,30 @@ void main()
 				max=a[i];
 			}
 	}
+	printf("\n*************************\n");
 	printf("\nMimimum value is:%d",min);
 	printf("\nMaximum value is:%d",max);
+
+	printf("\n*************************\n");
+
+	for(i=0;i<10;i++)
+	{
+		for(j=i+1;j<10;j++)
+		{
+			if(a[i]>a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+
+	printf("\nAfter Array is Ascending Order\n");
+	for(i=0;i<10;i++)
+	{
+		printf("\n%d",a[i]);
+	}
+
 	getch();
 }
